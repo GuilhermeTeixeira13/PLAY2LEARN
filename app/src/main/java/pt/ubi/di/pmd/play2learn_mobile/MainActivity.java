8188318bfd62b0ca93a863d02d812cdbf57cae72
Guilherme Teixeira <75620226+GuilhermeTeixeira13@users.noreply.github.com>
2022-11-20 21:49:56 +0000

    Initial commit

---
+++ app/src/main/java/pt/ubi/di/pmd/play2learn_mobile/MainActivity.java
@@ -0,0 +1,14 @@
+package pt.ubi.di.pmd.play2learn_mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
