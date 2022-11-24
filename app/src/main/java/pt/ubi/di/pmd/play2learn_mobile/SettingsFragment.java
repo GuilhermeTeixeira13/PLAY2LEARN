d1914c170b185572359a068838f02ba4f2bb1b8c
Guilherme Teixeira <75620226+GuilhermeTeixeira13@users.noreply.github.com>
2022-11-24 08:39:54 +0000

    [G - LIN 01]

--- app/src/main/java/pt/ubi/di/pmd/play2learn_mobile/SettingsFragment.java
+++ app/src/main/java/pt/ubi/di/pmd/play2learn_mobile/SettingsFragment.java
@@ -1,15 +1,27 @@
 package pt.ubi.di.pmd.play2learn_mobile;
 
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 
+import java.util.Locale;
+
 public class SettingsFragment extends Fragment {
+    Button btnPT;
+    Button btnEN;
+
+    static boolean isInit = true;
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
@@ -18,6 +30,64 @@ public class SettingsFragment extends Fragment {
         // Change toolbar title
         getActivity().setTitle(getResources().getString(R.string.SettingsFragment));
 
+        btnPT = view.findViewById(R.id.changePT);
+        btnEN = view.findViewById(R.id.changeENG);
+
+        // If it is the first time loading the view, it loads the last language used by the user
+        if(isInit) {
+            isInit = false;
+            loadPreviousLanguage();
+        }
+
+        // Change toolbar title
+        getActivity().setTitle(getResources().getString(R.string.app_name));
+
+        // Change languages by button clicks
+        btnPT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeLanguage("pt");
+            }
+        });
+
+        btnEN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeLanguage("en");
+            }
+        });
+
         return view;
+    }
+
+    // When the language is changed, it is saved using Shared Preferences
+    public void saveLanguage(String lang) {
+        String langPref = "Language";
+        SharedPreferences prefs = getActivity().getSharedPreferences("CommonPrefs", getActivity().MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(langPref, lang);
+        editor.commit();
+    }
+
+    // Loading the language that was previously saved in Shared Preferences
+    public void loadPreviousLanguage() {
+        // Loading
+        String langPref = "Language";
+        SharedPreferences prefs = getActivity().getSharedPreferences("CommonPrefs", getActivity().MODE_PRIVATE);
+        String language = prefs.getString(langPref, "");
+        changeLanguage(language);
+    }
+
+    // Change the app language to other one
+    public void changeLanguage(String lang) {
+        Locale myLocale = new Locale(lang);
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.locale = myLocale;
+        res.updateConfiguration(conf, dm);
+        getActivity().getResources().updateConfiguration(conf, getResources().getDisplayMetrics());
+        saveLanguage(lang);
+        getActivity().recreate();
     }
 }
