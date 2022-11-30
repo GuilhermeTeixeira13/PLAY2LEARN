d571fcfafb1c037fc9346f94adf155bc7d67512f
João Baltazar <joaobaltazar55@gmail.com>
2022-11-30 22:35:01 +0000

    .

--- app/src/main/java/pt/ubi/di/pmd/play2learn_mobile/BaseActivity.java
+++ app/src/main/java/pt/ubi/di/pmd/play2learn_mobile/BaseActivity.java
@@ -17,6 +17,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -35,6 +36,8 @@ public class BaseActivity extends AppCompatActivity implements NavigationView.On
     private DrawerLayout drawer;
     ImageView ProfileImage;
     TextView ProfileName;
+    EditText editTextNomeFriend;
+    private String nameuserlogged;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,6 +70,13 @@ public class BaseActivity extends AppCompatActivity implements NavigationView.On
                     new GameFragment()).commit();
             navigationView.setCheckedItem(R.id.nav_game);
         }
+
+        // MyGroup
+        Intent iCameFromMainNameUser = getIntent();
+        nameuserlogged = iCameFromMainNameUser.getStringExtra("name");
+        System.out.println("NOME: " + nameuserlogged);
+        editTextNomeFriend = (EditText) findViewById(R.id.add_player_name);
+        editTextNomeFriend.setText("teste");
 
     }
 
@@ -130,7 +140,69 @@ public class BaseActivity extends AppCompatActivity implements NavigationView.On
     }
 
     public void addToList(View view) {
-
+        System.out.println("AQUI VAI O TEXTO: " + editTextNomeFriend.getText());
+        BaseActivity.addFriendToGroup addFriend = new BaseActivity.addFriendToGroup();
+        addFriend.execute();
     }
 
+    private class addFriendToGroup extends AsyncTask<String,String,String>{
+        String nomeuser = "";
+        String z = "";
+        boolean isSuccess = false;
+
+        @Override
+        protected String doInBackground(String... strings) {
+            if (editTextNomeFriend.getText().toString().isEmpty()){
+                z= "You need to specify our name's friend";
+            }else {
+                nomeuser = editTextNomeFriend.getText().toString();
+                try {
+                    P2L_DbHelper connectNow = new P2L_DbHelper();
+                    Connection connectDB = connectNow.getConnection();
+
+                    if (connectDB== null){
+                        z = "Please check your internet connection";
+                    }else {
+                        String query = "select * from users where Name='"+nomeuser;
+
+                        Statement statement = connectDB.createStatement();
+
+                        ResultSet rs = statement.executeQuery(query);
+
+                        while (rs.next()){
+                            nameuserlogged = rs.getString(2);
+                            nameuserlogged = rs.getString(4);
+
+                            if(nameuserlogged.equals(nameuserlogged) && nameuserlogged.equals(nameuserlogged)){
+                                isSuccess = true;
+                                z = "Login successfull";
+                                System.out.println("Login successfull");
+                            }else {
+                                isSuccess = false;
+                                System.out.println("Login NOT successfull");
+                            }
+                        }
+
+                    }
+
+                } catch (SQLException e) {
+                    isSuccess = false;
+                    z = "Exceptions"+e;
+                }
+
+            }
+            return z;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            Toast.makeText(getBaseContext(),""+z,Toast.LENGTH_LONG).show();
+
+            if (isSuccess){
+                Intent intent = new Intent(BaseActivity.this, BaseActivity.class);
+                intent.putExtra("name", nameuserlogged);
+                startActivity(intent);
+            }
+        }
+    }
 }
