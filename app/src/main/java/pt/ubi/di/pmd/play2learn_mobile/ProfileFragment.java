3ab7ef6506b26f2d9f52ea31a9aa0f01fb1f704f
Tiago Ribeiro <95197303+TiagoRibeiro02@users.noreply.github.com>
2022-12-08 17:29:34 +0000

    Update ProfileFragment.java

--- app/src/main/java/pt/ubi/di/pmd/play2learn_mobile/ProfileFragment.java
+++ app/src/main/java/pt/ubi/di/pmd/play2learn_mobile/ProfileFragment.java
@@ -1,15 +1,29 @@
 package pt.ubi.di.pmd.play2learn_mobile;
 
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 
 public class ProfileFragment extends Fragment {
+    private final int PICK_IMAGE_REQUEST=99;
+    private Uri imagepath;
+    private Bitmap imageTostore;
+    String userLogged = "";
+    ImageView picuser;
+    EditText nameuser;
+    EditText biogra;
+    Button save;
+    P2L_DbHelper connectionhelper;
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
