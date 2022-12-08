771f9f1c938c6829fe7040cd3770e7ebaf98e4b6
Tiago Ribeiro <95197303+TiagoRibeiro02@users.noreply.github.com>
2022-12-08 17:45:42 +0000

    Update ProfileFragment.java

--- app/src/main/java/pt/ubi/di/pmd/play2learn_mobile/ProfileFragment.java
+++ app/src/main/java/pt/ubi/di/pmd/play2learn_mobile/ProfileFragment.java
@@ -1,7 +1,9 @@
 package pt.ubi.di.pmd.play2learn_mobile;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,12 +11,17 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 
+import java.sql.Blob;
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ProfileFragment extends Fragment {
     private final int PICK_IMAGE_REQUEST=99;
@@ -55,18 +62,18 @@ public class ProfileFragment extends Fragment {
 
         picuser.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                choseimage();
+                //choseimage();
             }
         });
-        save.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                try {
-                    save(view);
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+        //save.setOnClickListener(new View.OnClickListener() {
+        //    public void onClick(View v) {
+        //        try {
+        //            save(view);
+        //        } catch (SQLException e) {
+        //            e.printStackTrace();
+        //        }
+        //    }
+        //});
 
         return view;
     }
@@ -75,5 +82,61 @@ public class ProfileFragment extends Fragment {
         printbio printbio = new printbio();
         printbio.execute();
 
+    }
+
+    private class printbio extends AsyncTask<String,String,String> {
+        String bio;
+        //Blob pic;
+        String z = "";
+        boolean isSuccess = false;
+        @Override
+        protected String doInBackground(String... strings) {
+
+            try {
+                P2L_DbHelper connectNow = new P2L_DbHelper();
+                Connection connectDB = connectNow.getConnection();
+                System.out.println(connectDB);
+
+                if (connectDB== null){
+                    z = "Please check your internet connection";
+                }else {
+                    String query = "select Biblio from users where Name='"+userLogged+"'";
+
+                    Statement statement = connectDB.createStatement();
+
+                    ResultSet rs = statement.executeQuery(query);
+
+                    while (rs.next()){
+                        bio = rs.getString(1);
+                        System.out.println(bio);
+
+                        if (bio != null){
+                            isSuccess = true;
+                        }
+                        //pic =rs.getBlob(2);
+                        //if (pic!=null) {
+                        //    int blobLength = (int) pic.length();
+                        //    byte[] blobAsBytes = pic.getBytes(1, blobLength);
+                        //    Bitmap btm = BitmapFactory.decodeByteArray(blobAsBytes, 0, blobAsBytes.length);
+                        //    picuser.setImageBitmap(btm);
+                        //}
+
+                    }
+
+                }
+
+            } catch (SQLException e) {
+                z = "Exceptions"+e;
+            }
+            return z;
+        }
+        protected void onPostExecute(String s) {
+            Toast.makeText(getContext(),""+z,Toast.LENGTH_LONG).show();
+
+            if (isSuccess){
+                nameuser.setText(userLogged);
+                biogra.setText(bio);
+            }
+        }
     }
 }
