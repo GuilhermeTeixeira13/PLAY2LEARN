c656dde99a1fba103f7807e712856071d353b692
Tiago Ribeiro <95197303+TiagoRibeiro02@users.noreply.github.com>
2022-12-12 21:03:36 +0000

    Update P2L_DbHelper.java

--- app/src/main/java/pt/ubi/di/pmd/play2learn_mobile/P2L_DbHelper.java
+++ app/src/main/java/pt/ubi/di/pmd/play2learn_mobile/P2L_DbHelper.java
@@ -7,9 +7,9 @@ public class P2L_DbHelper {
 
     public Connection getConnection(){
         String dataBaseName = "play2learndb";
-        String dataBaseUser = "joao2";
+        String dataBaseUser = "tiago4";
         String dataBasePassword = "";
-        String url= "jdbc:mysql://192.168.1.75/"+dataBaseName; // Mudar para o IP DO PC
+        String url= "jdbc:mysql://192.168.1.71/"+dataBaseName; // Mudar para o IP DO PC
 
         try{
             databaseLink = DriverManager.getConnection(url, dataBaseUser, dataBasePassword);
